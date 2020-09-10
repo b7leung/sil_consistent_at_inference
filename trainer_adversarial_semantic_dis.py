559e8bf82eafd1c9eea3f7c32275c2742ed65812
b7leung <b7leung@ucsd.edu>
2020-09-10 23:26:52 +0000

    added pointnet branch, cached dataloaders

--- trainer_adversarial_semantic_dis.py
+++ trainer_adversarial_semantic_dis.py
@@ -18,16 +18,16 @@ import numpy as np
 from utils import utils
 from deformation.deformation_net_extended import DeformationNetworkExtended
 import deformation.losses as def_losses
-from deformation.semantic_discriminator_loss import compute_sem_dis_logits
-from adversarial.datasets import GenerationDataset, ShapenetRendersDataset
+from deformation.semantic_discriminator_loss import compute_render_sem_dis_logits, compute_points_sem_dis_logits
+from adversarial.datasets import GenerationDataset, ShapenetRendersDataset, ShapenetPointsDataset
 from deformation.semantic_discriminator_net import SemanticDiscriminatorNetwork
+from deformation.points_semantic_discriminator_net import PointsSemanticDiscriminatorNetwork
 from adversarial.datasets import gen_data_collate
-from eval_adversarial_semantic_dis import AdversarialDiscriminatorEval
 
 
 class AdversarialDiscriminatorTrainer():
 
-    def __init__(self, cfg_path, gpu_num, exp_name, light):
+    def __init__(self, cfg_path, gpu_num, exp_name, num_workers):
         self.cfg = utils.load_config(cfg_path, "configs/default.yaml")
         self.device = torch.device("cuda:"+str(gpu_num))
         self.batch_size = self.cfg["semantic_dis_training"]["batch_size"]
@@ -49,13 +49,11 @@ class AdversarialDiscriminatorTrainer():
         self.real_labels_dist = torch.distributions.Uniform(torch.tensor([(1.0-self.real_label_offset)-self.label_noise]), torch.tensor([(1.0-self.real_label_offset)]))
         self.fake_labels_dist = torch.distributions.Uniform(torch.tensor([0.0]), torch.tensor([0.0+self.label_noise]))
 
-        self.num_workers = 8
-        if light: self.num_workers = 8
+        self.num_workers = num_workers
         self.tqdm_out = utils.TqdmPrintEvery()
 
 
-    # given a tensor of batches of images, dimensions (b x w x h x c) or channel-first (b x c x w x h)
-    # saves a specified amount of them
+    # given a tensor of batches of images, dimensions (b x w x h x c) or channel-first (b x c x w x h) saves a specified amount of them into jpgs
     def save_tensor_img(self, tensor, channel_first, name_prefix, output_dir, save_num=5):
         img_transforms = transforms.Compose([transforms.ToPILImage()])
         if not os.path.exists(output_dir): os.makedirs(output_dir)
@@ -68,32 +66,6 @@ class AdversarialDiscriminatorTrainer():
             img_transforms(img_tensor).save(os.path.join(output_dir, name_prefix+"_{}.jpg".format(i)))
 
 
-    def setup_discriminator(self):
-        dis_type = self.cfg['semantic_dis_training']['dis_type']
-        if dis_type == "renders":
-            # dataloader
-            num_render = self.cfg["training"]["semantic_dis_num_render"]
-            shapenet_renders_dataset = ShapenetRendersDataset(self.cfg)
-            semantic_dis_loader = torch.utils.data.DataLoader(shapenet_renders_dataset, batch_size=self.batch_size * num_render, num_workers=self.num_workers, shuffle=True)
-            # network
-            semantic_dis_net = SemanticDiscriminatorNetwork(self.cfg)
-            if self.dis_weight_path != "":
-                semantic_dis_net.load_state_dict(torch.load(self.dis_weight_path))
-            semantic_dis_net.to(self.device)
-            #optimizer
-            semantic_dis_optimizer = optim.Adam(semantic_dis_net.parameters(), lr=self.cfg["semantic_dis_training"]["dis_learning_rate"], weight_decay=1e-2)
-            # loss
-            semantic_dis_logits = compute_sem_dis_logits
-        
-        elif dis_type == "pointnet":
-            pass
-
-        else:
-            raise ValueError("dis_type must be renders or pointnet")
-
-        return semantic_dis_loader, semantic_dis_net, semantic_dis_optimizer, semantic_dis_logits
-    
-
     def eval(self, deform_net, semantic_dis_net, semantic_dis_logits, output_dir_name):
         num_mesh_eval = self.cfg['semantic_dis_training']['num_mesh_eval']
         if num_mesh_eval == 0:
@@ -102,7 +74,7 @@ class AdversarialDiscriminatorTrainer():
         eval_output_path = os.path.join(self.training_output_dir, output_dir_name)
         if not os.path.exists(eval_output_path): os.makedirs(eval_output_path)
 
-        eval_generation_dataset = GenerationDataset(self.cfg, self.device)
+        eval_generation_dataset = GenerationDataset(self.cfg)
         eval_generation_loader = torch.utils.data.DataLoader(eval_generation_dataset, batch_size=1, num_workers=1, shuffle=False, collate_fn=gen_data_collate)
         if num_mesh_eval == -1: num_mesh_eval = len(eval_generation_loader) + 1
 
@@ -135,10 +107,46 @@ class AdversarialDiscriminatorTrainer():
         pickle.dump(loss_info, open(os.path.join(eval_output_path, "eval_loss_info.p"),"wb"))
 
 
+    def setup_discriminator(self):
+        dis_type = self.cfg['semantic_dis_training']['dis_type']
+        if dis_type == "renders":
+            # dataloader
+            num_render = self.cfg["training"]["semantic_dis_num_render"]
+            shapenet_renders_dataset = ShapenetRendersDataset(self.cfg)
+            semantic_dis_loader = torch.utils.data.DataLoader(shapenet_renders_dataset, batch_size=self.batch_size * num_render, num_workers=self.num_workers, shuffle=True)
+            # network
+            semantic_dis_net = SemanticDiscriminatorNetwork(self.cfg)
+            if self.dis_weight_path != "":
+                semantic_dis_net.load_state_dict(torch.load(self.dis_weight_path))
+            semantic_dis_net.to(self.device)
+            #optimizer
+            semantic_dis_optimizer = optim.Adam(semantic_dis_net.parameters(), lr=self.cfg["semantic_dis_training"]["dis_learning_rate"], weight_decay=1e-2)
+            # loss
+            semantic_dis_logits = compute_render_sem_dis_logits
+        
+        elif dis_type == "pointnet":
+            # dataloader
+            shapenet_points_dataset = ShapenetPointsDataset(self.cfg)
+            semantic_dis_loader = torch.utils.data.DataLoader(shapenet_points_dataset, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=True)
+            #network
+            semantic_dis_net = PointsSemanticDiscriminatorNetwork(self.cfg)
+            if self.dis_weight_path != "":
+                semantic_dis_net.load_state_dict(torch.load(self.dis_weight_path))
+            semantic_dis_net.to(self.device)
+            #optimizer
+            semantic_dis_optimizer = optim.Adam(semantic_dis_net.parameters(), lr=self.cfg["semantic_dis_training"]["dis_learning_rate"], weight_decay=1e-2)
+            # loss
+            semantic_dis_logits = compute_points_sem_dis_logits
+        else:
+            raise ValueError("dis_type must be renders or pointnet")
+
+        return semantic_dis_loader, semantic_dis_net, semantic_dis_optimizer, semantic_dis_logits
+    
+
     def train(self):
 
         # setting up generator components 
-        generation_dataset = GenerationDataset(self.cfg, self.device)
+        generation_dataset = GenerationDataset(self.cfg)
         generation_loader = torch.utils.data.DataLoader(generation_dataset, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=True, collate_fn=gen_data_collate, drop_last=True)
         deform_net = DeformationNetworkExtended(self.cfg, self.cfg["semantic_dis_training"]["mesh_num_verts"], self.device)
         if self.gen_weight_path != "":
@@ -149,10 +157,10 @@ class AdversarialDiscriminatorTrainer():
         # setting up discriminator components
         semantic_dis_loader, semantic_dis_net, semantic_dis_optimizer, semantic_dis_logits = self.setup_discriminator()
 
+
         # training generative deformation network and discriminator in an alternating, GAN style
         training_df = {"deform_net_gen": pd.DataFrame(), "semantic_dis": pd.DataFrame()}
         for iter_idx in tqdm(range(self.total_training_iters), desc="Alternating MiniMax Iterations"):
-
             # training discriminator; generator weights are frozen
             ############################################################################################################################
             for param in semantic_dis_net.parameters(): param.requires_grad = True
@@ -160,7 +168,6 @@ class AdversarialDiscriminatorTrainer():
             for dis_epoch in tqdm(range(self.dis_epochs_per_iteration), desc="Iteration {} Discriminator Trainings".format(iter_idx)):
                 for batch_idx, (gen_batch, real_batch) in enumerate(tqdm(zip(generation_loader, semantic_dis_loader),
                                                                          total=min(len(generation_loader), len(semantic_dis_loader)), desc="Discriminator Epoch {} Batches".format(dis_epoch))):
-
                     semantic_dis_net.train()
                     deform_net.eval() # not sure if supposed to set this
                     semantic_dis_optimizer.zero_grad()
@@ -181,7 +188,6 @@ class AdversarialDiscriminatorTrainer():
                     batch_size = real_batch.shape[0]
                     real_labels = self.real_labels_dist.sample((batch_size,1)).squeeze(2).to(self.device)
                     fake_labels = self.fake_labels_dist.sample((batch_size,1)).squeeze(2).to(self.device)
-
                     dis_loss = F.binary_cross_entropy_with_logits(pred_logits_real, real_labels) + \
                         F.binary_cross_entropy_with_logits(pred_logits_fake, fake_labels)
 
@@ -190,8 +196,8 @@ class AdversarialDiscriminatorTrainer():
 
                     # compute accuracy & save to dataframe
                     batch_accuracies = []
-                    real_correct_vec = (torch.sigmoid(pred_logits_real) > 0.5) == real_labels.byte()
-                    fake_correct_vec = (torch.sigmoid(pred_logits_fake) > 0.5) == fake_labels.byte()
+                    real_correct_vec = (torch.sigmoid(pred_logits_real) > 0.5) == (real_labels>0.5)
+                    fake_correct_vec = (torch.sigmoid(pred_logits_fake) > 0.5) == (fake_labels>0.5)
                     batch_accuracies.append(real_correct_vec.cpu().numpy())
                     batch_accuracies.append(fake_correct_vec.cpu().numpy())
                     batch_accuracy = np.mean(np.concatenate(batch_accuracies, axis=0)).item()
@@ -203,7 +209,7 @@ class AdversarialDiscriminatorTrainer():
                         img_output_dir = os.path.join(self.training_output_dir, "training_saved_images", "iter_{}".format(iter_idx))
                         self.save_tensor_img(real_batch, True, "iter_{}_real".format(iter_idx), img_output_dir, 32)
                         self.save_tensor_img(semantic_dis_debug_data, False, "iter_{}_fake".format(iter_idx), img_output_dir, 32)
-
+                pickle.dump(training_df, open(os.path.join(self.training_output_dir, "training_df.p"),"wb"))
 
             # training generator; discriminator weights are frozen
             ############################################################################################################################
@@ -211,7 +217,6 @@ class AdversarialDiscriminatorTrainer():
             for param in deform_net.parameters(): param.requires_grad = True
             for gen_epoch in tqdm(range(self.gen_epochs_per_iteration), desc="Iteration {} Generator Trainings".format(iter_idx)):
                 for batch_idx, gen_batch in enumerate(tqdm(generation_loader, desc="Generator Epoch {} Batches".format(gen_epoch))):
-
                     gen_batch_meshes = gen_batch["mesh"].to(self.device)
                     gen_batch_vertices = gen_batch["mesh_verts"].to(self.device)
                     gen_batch_images = gen_batch["image"].to(self.device)
@@ -308,11 +313,14 @@ class AdversarialDiscriminatorTrainer():
 # python adversarial_semantic_dis_trainer.py --exp_name test --light
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Adversarially train a SemanticDiscriminatorNetwork.')
-    parser.add_argument('--cfg_path', type=str, default="configs/default.yaml", help='Path to yaml configuration file.')
+    parser.add_argument('--cfg', type=str, default="configs/default.yaml", help='Path to yaml configuration file.')
     parser.add_argument('--gpu', type=int, default=0, help='Gpu number to use.')
     parser.add_argument('--exp_name', type=str, default="adv_semantic_discrim", help='name of experiment')
-    parser.add_argument('--light', action='store_true', help='run a lighter version of training w/ smaller batch size and num_workers')
+    parser.add_argument('--num_workers', type=int, default=8, help='num_workers to use')
     args = parser.parse_args()
 
-    trainer = AdversarialDiscriminatorTrainer(args.cfg_path, args.gpu, args.exp_name, args.light)
+    start_time = time.time()
+    trainer = AdversarialDiscriminatorTrainer(args.cfg, args.gpu, args.exp_name, args.num_workers)
     training_df = trainer.train()
+    end_time = time.time()
+    print("\n \n >>> Finished training in {} seconds. <<< \n".format(end_time - start_time))
