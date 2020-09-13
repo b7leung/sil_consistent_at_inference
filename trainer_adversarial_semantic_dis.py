9e9e22cc7baf06fab0c61ef32a6f154bd1ce203d
b7leung <b7leung@ucsd.edu>
2020-09-13 10:26:19 +0000

    checkpoint before v2 experiments

--- trainer_adversarial_semantic_dis.py
+++ trainer_adversarial_semantic_dis.py
@@ -36,6 +36,7 @@ class AdversarialDiscriminatorTrainer():
         self.total_training_iters = self.cfg["semantic_dis_training"]["adv_iterations"]
         self.dis_epochs_per_iteration = self.cfg["semantic_dis_training"]["dis_epochs_per_iteration"]
         self.gen_epochs_per_iteration = self.cfg["semantic_dis_training"]["gen_epochs_per_iteration"]
+        self.early_stop_dis_acc = self.cfg["semantic_dis_training"]["early_stop_dis_acc"]
         self.save_model_every = self.cfg["semantic_dis_training"]["save_model_every"]
 
         # creating output dir
@@ -79,7 +80,7 @@ class AdversarialDiscriminatorTrainer():
         if num_mesh_eval == -1: num_mesh_eval = len(eval_generation_loader) + 1
 
         loss_info = {}
-        with tqdm(total=num_mesh_eval, desc="Evaluating Meshes") as pbar:
+        with tqdm(total=num_mesh_eval, desc="Evaluating Meshes", leave=False) as pbar:
             for batch_idx, gen_batch in enumerate(eval_generation_loader):
                 if batch_idx >= num_mesh_eval: break
 
@@ -119,12 +120,14 @@ class AdversarialDiscriminatorTrainer():
             if self.dis_weight_path != "":
                 semantic_dis_net.load_state_dict(torch.load(self.dis_weight_path))
             semantic_dis_net.to(self.device)
-            #optimizer
-            semantic_dis_optimizer = optim.Adam(semantic_dis_net.parameters(), lr=self.cfg["semantic_dis_training"]["dis_learning_rate"], weight_decay=1e-2)
+            #optimizer dis_points_lr
+            lr = self.cfg["semantic_dis_training"]["dis_renders_lr"]
+            decay = self.cfg["semantic_dis_training"]["dis_renders_decay"]
+            semantic_dis_optimizer = optim.Adam(semantic_dis_net.parameters(), lr=lr, weight_decay=decay)
             # loss
             semantic_dis_logits = compute_render_sem_dis_logits
         
-        elif dis_type == "pointnet":
+        elif dis_type == "points":
             # dataloader
             shapenet_points_dataset = ShapenetPointsDataset(self.cfg)
             semantic_dis_loader = torch.utils.data.DataLoader(shapenet_points_dataset, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=True)
@@ -134,14 +137,20 @@ class AdversarialDiscriminatorTrainer():
                 semantic_dis_net.load_state_dict(torch.load(self.dis_weight_path))
             semantic_dis_net.to(self.device)
             #optimizer
-            semantic_dis_optimizer = optim.Adam(semantic_dis_net.parameters(), lr=self.cfg["semantic_dis_training"]["dis_learning_rate"], weight_decay=1e-2)
+            lr = self.cfg["semantic_dis_training"]["dis_points_lr"]
+            decay = self.cfg["semantic_dis_training"]["dis_points_decay"]
+            semantic_dis_optimizer = optim.Adam(semantic_dis_net.parameters(), lr=lr, weight_decay=decay)
             # loss
             semantic_dis_logits = compute_points_sem_dis_logits
+            #TODO: normalize + data aug on point sets?
         else:
             raise ValueError("dis_type must be renders or pointnet")
 
         return semantic_dis_loader, semantic_dis_net, semantic_dis_optimizer, semantic_dis_logits
-    
+
+
+    #def setup_generator(self):
+    #    return generation_loader, deform_net, deform_optimizer,  
 
     def train(self):
 
@@ -152,22 +161,30 @@ class AdversarialDiscriminatorTrainer():
         if self.gen_weight_path != "":
             deform_net.load_state_dict(torch.load(self.gen_weight_path))
         deform_net.to(self.device)
-        deform_optimizer = optim.Adam(deform_net.parameters(), lr=self.cfg["training"]["learning_rate"])
+        deform_optimizer = optim.Adam(deform_net.parameters(), lr=self.cfg["training"]["learning_rate"], weight_decay=self.cfg['model']['gen_decay'])
 
         # setting up discriminator components
         semantic_dis_loader, semantic_dis_net, semantic_dis_optimizer, semantic_dis_logits = self.setup_discriminator()
 
-
         # training generative deformation network and discriminator in an alternating, GAN style
         training_df = {"deform_net_gen": pd.DataFrame(), "semantic_dis": pd.DataFrame()}
-        for iter_idx in tqdm(range(self.total_training_iters), desc="Alternating MiniMax Iterations"):
+        for iter_idx in tqdm(range(self.total_training_iters), desc="Alternating MiniMax Iterations", leave=False):
             # training discriminator; generator weights are frozen
             ############################################################################################################################
             for param in semantic_dis_net.parameters(): param.requires_grad = True
             for param in deform_net.parameters(): param.requires_grad = False
-            for dis_epoch in tqdm(range(self.dis_epochs_per_iteration), desc="Iteration {} Discriminator Trainings".format(iter_idx)):
-                for batch_idx, (gen_batch, real_batch) in enumerate(tqdm(zip(generation_loader, semantic_dis_loader),
+            dis_batch_acc_history = []
+            early_stop = False
+            for dis_epoch in tqdm(range(self.dis_epochs_per_iteration), desc="Iteration {} Discriminator Trainings".format(iter_idx), leave=False):
+                for batch_idx, (gen_batch, real_batch) in enumerate(tqdm(zip(generation_loader, semantic_dis_loader), leave=False,
                                                                          total=min(len(generation_loader), len(semantic_dis_loader)), desc="Discriminator Epoch {} Batches".format(dis_epoch))):
+
+                    # if early stop, just fill the rest of the dataframe with last recorded value
+                    if early_stop:
+                        curr_train_info = {"iteration": iter_idx, "batch": batch_idx, "semantic_dis_loss": dis_loss.item(), "batch_avg_dis_acc": batch_accuracy}
+                        training_df["semantic_dis"] = training_df["semantic_dis"].append(curr_train_info, ignore_index=True)
+                        continue
+
                     semantic_dis_net.train()
                     deform_net.eval() # not sure if supposed to set this
                     semantic_dis_optimizer.zero_grad()
@@ -204,19 +221,35 @@ class AdversarialDiscriminatorTrainer():
                     curr_train_info = {"iteration": iter_idx, "batch": batch_idx, "semantic_dis_loss": dis_loss.item(), "batch_avg_dis_acc": batch_accuracy}
                     training_df["semantic_dis"] = training_df["semantic_dis"].append(curr_train_info, ignore_index=True)
 
+                    # keeping running average of past 10 discriminator accuracies, and early stop if needed
+                    if self.early_stop_dis_acc != -1:
+                        dis_batch_acc_history.insert(0,batch_accuracy)
+                        #tqdm.write(str(dis_batch_acc_history))
+                        if len(dis_batch_acc_history) > 10:
+                            dis_batch_acc_history.pop()
+                            dis_batch_avg_acc = np.average(dis_batch_acc_history)
+                            if dis_batch_avg_acc >= self.early_stop_dis_acc:
+                                early_stop = True
+                                tqdm.write("Iter. {}: Early stopped dis. training at epoch {}, batch {}, with avg acc of {}.".format(iter_idx, dis_epoch, batch_idx, dis_batch_avg_acc))
+
                     # if discriminator is render based, save some example inputs to discriminator from the first batch
                     if self.cfg['semantic_dis_training']['dis_type'] == "renders" and dis_epoch == 0 and batch_idx == 0:
                         img_output_dir = os.path.join(self.training_output_dir, "training_saved_images", "iter_{}".format(iter_idx))
                         self.save_tensor_img(real_batch, True, "iter_{}_real".format(iter_idx), img_output_dir, 32)
                         self.save_tensor_img(semantic_dis_debug_data, False, "iter_{}_fake".format(iter_idx), img_output_dir, 32)
+
+                if training_df["semantic_dis"].isnull().values.any():
+                    tqdm.write("WARNING: nan in dataframe.")
+                    raise ZeroDivisionError("nan in dataframe")
                 pickle.dump(training_df, open(os.path.join(self.training_output_dir, "training_df.p"),"wb"))
+
 
             # training generator; discriminator weights are frozen
             ############################################################################################################################
             for param in semantic_dis_net.parameters(): param.requires_grad = False
             for param in deform_net.parameters(): param.requires_grad = True
-            for gen_epoch in tqdm(range(self.gen_epochs_per_iteration), desc="Iteration {} Generator Trainings".format(iter_idx)):
-                for batch_idx, gen_batch in enumerate(tqdm(generation_loader, desc="Generator Epoch {} Batches".format(gen_epoch))):
+            for gen_epoch in tqdm(range(self.gen_epochs_per_iteration), desc="Iteration {} Generator Trainings".format(iter_idx), leave=False):
+                for batch_idx, gen_batch in enumerate(tqdm(generation_loader, desc="Generator Epoch {} Batches".format(gen_epoch), leave=False)):
                     gen_batch_meshes = gen_batch["mesh"].to(self.device)
                     gen_batch_vertices = gen_batch["mesh_verts"].to(self.device)
                     gen_batch_images = gen_batch["image"].to(self.device)
@@ -238,6 +271,9 @@ class AdversarialDiscriminatorTrainer():
                     curr_train_info = {**curr_train_info, **{loss_name:loss_dict[loss_name].item() for loss_name in loss_dict}}
                     training_df["deform_net_gen"] = training_df["deform_net_gen"].append(curr_train_info, ignore_index=True)
 
+                if training_df["deform_net_gen"].isnull().values.any():
+                    tqdm.write("WARNING: nan in dataframe.")
+                    raise ZeroDivisionError("nan in dataframe")
                 pickle.dump(training_df, open(os.path.join(self.training_output_dir, "training_df.p"),"wb"))
 
             # save network parameters and evaluate meshes using current network
